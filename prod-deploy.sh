cf2e6145673c522570d5ba6036230ac2f178d4b0
dallasw <dallas.watterson@outlook.com>
2022-06-02 23:51:06 -0500

    WIP of working on release build

--- prod-deploy.sh
+++ prod-deploy.sh
@@ -1,10 +1,26 @@
 #!/bin/zsh
 
-# Build the Client image
-docker build --target production -t payme-client:latest ./PayMe.Client
+# Build our contains for FE and BE
+docker build --target production -t watterson.azurecr.io/payme-client ./PayMe.Client &&
+docker build -t watterson.azurecr.io/payme-server -f PayMe.Server/Dockerfile . &&
 
+# Push our images to our registry
+docker login watterson.azurecr.io
+docker push watterson.azurecr.io/payme-client
+docker push watterson.azurecr.io/payme-server
 
-# docker build -t paymeapp -f PayMe.Server/Dockerfile . &&
-# kubectl apply -f ./local-deployment.yaml &&
-# kubectl rollout restart deployment/paymeapp &&
-# kubectl get services
+# Deploy everything to k8s
+kubectl apply -f ./PayMe.Infra/prod/redis-deployment.yaml &&
+kubectl apply -f ./PayMe.Infra/prod/redis-service.yaml &&
+kubectl apply -f ./PayMe.Infra/prod/roles.yaml &&
+kubectl apply -f ./PayMe.Infra/prod/backend-deployment.yaml &&
+kubectl apply -f ./PayMe.Infra/prod/backend-service.yaml &&
+kubectl apply -f ./PayMe.Infra/prod/frontend-deployment.yaml &&
+kubectl apply -f ./PayMe.Infra/prod/frontend-service.yaml &&
+
+#kubectl rollout restart deployment/redis &&
+kubectl rollout restart deployment/backend &&
+kubectl rollout restart deployment/frontend &&
+
+# See what's running
+kubectl get services
